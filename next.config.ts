735b081e4db5296dd32d3413c2f236900d655c4a
axergt180-ops <axergt180@github.com>
2025-12-17 16:35:02 +0700

    V 1.1.4

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
-// next.config.ts
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Enable static export
   images: {
-    unoptimized: true, // Required for static export
+    unoptimized: true,
   },
-  trailingSlash: true,
 };
 
 export default nextConfig;
